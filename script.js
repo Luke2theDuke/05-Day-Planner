873392d3cef3ee9b2259e7fb0dce42f406e1f787
Luke2theDuke <57816716+Luke2theDuke@users.nopreply.github.com>
2019-12-20 23:26:14 -0600

    Local storage working on all rows.  Fixed CSS issue with Jumbotron

--- script.js
+++ script.js
@@ -39,7 +39,23 @@ $(".time-block").each(rowStyle);
 // Function that sets up variables for the values of the text area input boxes and makes the values persistent on page reload
 function storageData() {
     const nine = localStorage.getItem("nine");
-
+    const ten = localStorage.getItem("ten");
+    const eleven = localStorage.getItem("eleven");
+    const twelve = localStorage.getItem("twelve");
+    const one = localStorage.getItem("one");
+    const two = localStorage.getItem("two");
+    const three = localStorage.getItem("three");
+    const four = localStorage.getItem("four");
+    const five = localStorage.getItem("five");
+    $("#nine").find("textarea").text(nine)
+    $("#ten").find("textarea").text(ten)
+    $("#eleven").find("textarea").text(eleven)
+    $("#twelve").find("textarea").text(twelve)
+    $("#one").find("textarea").text(one)
+    $("#two").find("textarea").text(two)
+    $("#three").find("textarea").text(three)
+    $("#four").find("textarea").text(four)
+    $("#five").find("textarea").text(five)
 }
 
 // Function that stores the key and value into local storage on save button click
